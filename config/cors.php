bb5b75eaff72bd28727c15381830a77270e6a07c
Sushma Chaudhary <susmachaudhary04@gmail.com>
2023-11-01 15:14:08 +0545

    updated cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://memesmaza.com/', 'http://localhost:3000'],
+    'allowed_origins' => ['https://santrash.com/', 'http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
