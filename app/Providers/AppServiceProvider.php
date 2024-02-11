1e561f063b3b59887cf69dc832dd98d8f48c40dd
shailu214 <134007465+shailu214@users.noreply.github.com>
2024-02-11 23:24:41 +0530

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if(env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');            
+        }
     }
 
     /**
