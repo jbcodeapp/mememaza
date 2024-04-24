27651644f3445f326a9feed37c6a00485823fb39
rohitjbcodeapp <139564077+rohitjbcodeapp@users.noreply.github.com>
2024-04-24 19:54:23 +0530

    seeder in category and post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,7 +25,7 @@ class DatabaseSeeder extends Seeder
         ]);
 
         \App\Models\User::factory(20)->create();
-        \App\Models\Category::factory(10)->create();
-        \App\Models\Post::factory(100)->create();
+        // \App\Models\Category::factory(10)->create();
+        // \App\Models\Post::factory(100)->create();
     }
 }
